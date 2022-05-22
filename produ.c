3f197617211029532a267d64b716eb921d6af804
revv-tech <52094428+revv-tech@users.noreply.github.com>
2022-05-22 13:50:17 -0600

    tentativo

--- produ.c
+++ produ.c
@@ -8,15 +8,9 @@
 #include <pthread.h>
 #include <stdbool.h>
 #include <semaphore.h> 
-<<<<<<< HEAD
-
-
-//#include "linea.h"
-=======
 #include <stdio.h>
 
 
->>>>>>> Revvro
 #include "proceso.h"
 
 
@@ -26,8 +20,6 @@ int * control_address;
 Proceso * estados_address;
 sem_t sem_ready, sem_estados;
 
-<<<<<<< HEAD
-=======
 // Auxiliar para obtener la cantidad de procesos en el Queue
 int getQueueSize(int queue[10]) {
     int i = 0;
@@ -64,7 +56,6 @@ char *join(char *dest, size_t size, const int *array, size_t count) {
     return dest;
 }
 
->>>>>>> Revvro
 /**
     Retorna un numero entero aleatorio entre [min, max]
 */
@@ -84,11 +75,7 @@ int agregarProceso(Proceso * proceso){
 
     int pos = control_address[2];                   //cantidad de procesos vivos
     estados_address[pos].pid = proceso->pid;
-<<<<<<< HEAD
-    estados_address[pos].tamano = proceso->tamano;
-=======
     estados_address[pos].cant_pags = proceso->cant_pags;
->>>>>>> Revvro
     estados_address[pos].tiempo = proceso->tiempo;
     
     control_address[2] = control_address[2] + 1;    //incremento el contador de procesos vivos
@@ -142,38 +129,22 @@ void escBitacora(const char *texto){
 
 
 //============================================================
-<<<<<<< HEAD
-//          ALGORITMOS DE PAGINACION Y SEGMENTACION
-=======
 //          ALGORITMOS DE ProcesoCION Y SEGMENTACION
->>>>>>> Revvro
 //============================================================
 
 /*
      Algoritmo Paginacion
 */
-<<<<<<< HEAD
-void * first_fit(void * arg){
-    
-    struct Proceso * temp;
-    temp = (struct Proceso *)arg;
-    temp->pid = pthread_self();
-=======
 void * asignarEspacio_Paginacion(void * arg){
     
     struct Proceso * temp;
     temp = (struct Proceso *)arg;
     temp->pid = control_address[3];
->>>>>>> Revvro
 
     // agrego el proceso actual a los procesos vivos
     int pos_estado = agregarProceso(temp);
     //guardo los valores para accederlos más facil
-<<<<<<< HEAD
-    int tamano = estados_address[pos_estado].tamano;
-=======
     int cant_pags_proceso = estados_address[pos_estado].cant_pags;
->>>>>>> Revvro
     int tiempo = estados_address[pos_estado].tiempo;
     long int pid = estados_address[pos_estado].pid;
     estados_address[pos_estado].estado = Bloqueado;
@@ -183,15 +154,11 @@ void * asignarEspacio_Paginacion(void * arg){
 
     int pos = 0;
     bool ocupado;
-<<<<<<< HEAD
-    bool insertado = false;
-=======
     bool asignado = false;
 
     // Array de rastreo de paginas
 
     int bitacoraPaginas[10] = {-1,-1,-1,-1,-1,-1,-1,-1,-1,-1};
->>>>>>> Revvro
 
     // pide el semáforo
     sem_wait(&sem_ready);
@@ -203,326 +170,6 @@ void * asignarEspacio_Paginacion(void * arg){
     //Valores para la bitácora
     char resultado[100];
 
-<<<<<<< HEAD
-    while(pos <= (control_address[0] - tamano)){        // control_address[0] = cantidad de lineas
-        ocupado = false;
-
-        // para saltar más rapido las que estan ocupadas
-        while(mem_address[pos].pid != -1)
-            pos++;
-
-        for(int i=pos; i<(pos + tamano); i++){
-            if(mem_address[i].pid != -1){
-                ocupado = true;
-                pos = i + 1;
-                break;
-            }
-        }
-
-        if(!ocupado && (pos <= control_address[0] - tamano)){
-            for(int i=pos; i<(pos + tamano); i++){
-                mem_address[i] = estados_address[pos_estado];
-            }
-            insertado = true;
-            break;
-        }
-    }
-
-    if(insertado){
-    	//Escribe en la bitácora
-    	sprintf(resultado, ": Proceso: Asignación. Hilo: %ld. Filas Ocupadas:%d-%d. Tamaño %d. Tiempo %d\n", pid, pos, pos+tamano-1, tamano, tiempo);
-    	escBitacora(resultado);
-        // devuelve el semáforo
-        sem_post(&sem_ready);
-
-        estados_address[pos_estado].estado = Ejecutando;
-        
-	   //printf("Ejecutando...\n");
-        sleep(1);//para notarlo en el espia ¡NO BORRAR!
-	        
-        printf("(Entró %ld: tamaño %d, tiempo %d)\n", pid, tamano, tiempo);
-        
-        sleep(tiempo);// "Ejecuta"
-
-        // pide el semáforo
-        sem_wait(&sem_ready);
-
-    	//Escribe en la bitácora
-    	sprintf(resultado, ": Proceso: Desasignación. Hilo: %ld. Filas Ocupadas:%d-%d. Tamaño %d. Tiempo %d\n",
-             pid, pos, pos+tamano-1, tamano, tiempo);
-    	escBitacora(resultado);
-
-        // devuelve los recursos
-        for(int i=pos; i<(pos + tamano); i++){
-            mem_address[i].pid = -1;
-        }
-
-    }else{
-        sprintf(resultado, ": Proceso: Defunción. El hilo %ld de tamaño %d, murió. Motivo: No encontró amor\n",
-             pid, tamano);
-        printf("-El hilo %ld de tamaño %d, murió porque no encontró amor\n",
-                 pid, tamano);
-        escBitacora(resultado);
-    }
-
-    // elimino el proceso actual de los procesos vivos
-    eliminarProceso(pid);
-
-    // devuelve el semáforo
-    sem_post(&sem_ready);
-
-    return NULL;
-}
-
-
-
-void * best_fit(void * arg){
-    struct Proceso * temp;
-    temp = (struct Proceso *)arg;
-    temp->pid = pthread_self();
-
-    // agrego el proceso actual a los procesos vivos
-    int pos_estado = agregarProceso(temp);
-    //guardo los valores para accederlos más facil
-    int tamano = estados_address[pos_estado].tamano;
-    int tiempo = estados_address[pos_estado].tiempo;
-    long int pid = estados_address[pos_estado].pid;
-    estados_address[pos_estado].estado = Bloqueado;
-
-    //printf("Bloqueado... ");
-    sleep(1);      //para notarlo en el espia ¡NO BORRAR!
-    
-    //Valores para la bitácora
-    char resultado[100];
-
-    //Valores para el Bestfit
-    int iPos = -1;
-    int fPos = -1;
-    int espacio = 0;
-
-    int pos = 0;
-    bool ocupado;
-    bool insertado = false;
-
-    // pide el semáforo
-    sem_wait(&sem_ready);
-    estados_address[pos_estado].estado = RegionCritica;
-
-    //printf("RC... ");
-    sleep(1);         //para notarlo en el espia ¡NO BORRAR!
-
-    while(pos <= (control_address[0] - tamano)){//control_address[0] = cantidad de lineas
-        ocupado = false;
-
-        // para saltar más rapido las que estan ocupadas
-        while(mem_address[pos].pid != -1){
-            pos++;
-        }
-
-        for(int i=pos; i<(pos + tamano); i++){
-            if(mem_address[i].pid != -1){
-                ocupado = true;
-                pos = i + 1;
-                break;
-            }
-        }
-
-        if(!ocupado && (pos <= control_address[0] - tamano)){
-    	    int cantEspacio = 0;
-    	    int contador = pos;
-    	    while(mem_address[contador].pid == -1){
-    		  contador = contador + 1;
-    		  cantEspacio = cantEspacio + 1;
-    	    }
-    	    if (pos>iPos && pos>fPos){
-        		if(espacio == 0) {
-        		    espacio = cantEspacio;
-        		    iPos = pos;
-        		    fPos = iPos + cantEspacio;
-        		}else{ 
-        		    if(cantEspacio >= tamano && cantEspacio < espacio){
-        			espacio = cantEspacio;
-        			iPos = pos;
-        			fPos = iPos + cantEspacio;
-        		    }
-        		}
-    	    }
-    	}
-
-    	pos = pos + 1; 
-    }
-
-    if(iPos>=0){
-        for(int i=iPos; i<(iPos + tamano); i++){
-            mem_address[i] = estados_address[pos_estado];
-        }
-        insertado = true;
-    }
-
-    if(insertado){
-    	// Escribe en bitácora
-    	sprintf(resultado, ": Proceso: Asignación. Hilo: %ld. Filas Ocupadas:%d-%d. Tamaño %d. Tiempo %d\n",
-             pid, iPos, iPos+tamano-1, tamano, tiempo);
-    	escBitacora(resultado);
-        
-	   // devuelve el semáforo
-        sem_post(&sem_ready);
-
-        estados_address[pos_estado].estado = Ejecutando;
-
-        //printf("Ejecutando...\n");
-        sleep(1);         //para notarlo en el espia ¡NO BORRAR!
-        printf("(Entró %ld: tamaño %d, tiempo %d)\n", pid, tamano, tiempo);
-        sleep(tiempo);              // "Ejecuta"
-
-        // pide el semáforo
-        sem_wait(&sem_ready);
-
-    	// Escribe en bitácora
-    	sprintf(resultado, ": Proceso: Desasignación. Hilo: %ld. Filas Ocupadas:%d-%d. Tamaño %d. Tiempo %d\n",
-             pid, iPos, iPos+tamano-1, tamano, tiempo);
-    	escBitacora(resultado);
-
-        // devuelve los recursos
-        for(int i=iPos; i<(iPos + tamano); i++){
-            mem_address[i].pid = -1;
-        }
-
-    }else{
-        sprintf(resultado, ": Proceso: Defunción. El hilo %ld de tamaño %d, murió. Motivo: No encontró amor\n",
-             pid, tamano);
-        printf("-El hilo %ld de tamaño %d, murió porque no encontró amor\n", pid, tamano);
-        escBitacora(resultado);
-    }
-
-    // elimino el proceso actual de los procesos vivos
-    eliminarProceso(pid);
-
-    // devuelve el semáforo
-    sem_post(&sem_ready);
-
-    return NULL;
-}
-
-
-void * worst_fit(void * arg){
-    struct Proceso * temp;
-    temp = (struct Proceso *)arg;
-    temp->pid = pthread_self();
-
-    // agrego el proceso actual a los procesos vivos
-    int pos_estado = agregarProceso(temp);
-    //guardo los valores para accederlos más facil
-    int tamano = estados_address[pos_estado].tamano;
-    int tiempo = estados_address[pos_estado].tiempo;
-    long int pid = estados_address[pos_estado].pid;
-    estados_address[pos_estado].estado = Bloqueado;
-
-    //printf("Bloqueado... ");
-    sleep(1);      //para notarlo en el espia ¡NO BORRAR!
-
-    //Valores para la bitácora
-    char resultado[100];
-
-    //Valores para el Worstfit
-    int iPos = -1;
-    int fPos = -1;
-    int espacio = 0;
-
-    int pos = 0;
-    bool ocupado;
-    bool insertado = false;
-
-    // pide el semáforo
-    sem_wait(&sem_ready);
-    estados_address[pos_estado].estado = RegionCritica;
-
-    //printf("RC... ");
-    sleep(1);         //para notarlo en el espia ¡NO BORRAR!
-
-    while(pos <= (control_address[0] - tamano)){//control_address[0] = cantidad de lineas
-        ocupado = false;
-
-        // para saltar más rapido las que estan ocupadas
-        while(mem_address[pos].pid != -1){
-            pos++;
-        }
-
-        for(int i=pos; i<(pos + tamano); i++){
-            if(mem_address[i].pid != -1){
-                ocupado = true;
-                pos = i + 1;
-                break;
-            }
-        }
-
-        if(!ocupado && (pos <= control_address[0] - tamano)){
-    	    int cantEspacio = 0;
-    	    int contador = pos;
-    	    while(mem_address[contador].pid == -1){
-                contador = contador + 1;
-                cantEspacio = cantEspacio + 1;
-    	    }
-    	    if (pos>iPos && pos>fPos){
-        		if(espacio == 0) {
-        		    espacio = cantEspacio;
-        		    iPos = pos;
-        		    fPos = iPos + cantEspacio;
-        		}else{ 
-        		    if(cantEspacio >= tamano && cantEspacio > espacio){
-                        printf("aqui\n");
-                        espacio = cantEspacio;
-            			iPos = pos;
-            			fPos = iPos + cantEspacio;
-        		    }
-        		}
-    	    }
-    	}
-
-    	pos = pos + 1; 
-    }
-
-    if(iPos>=0){
-        for(int i=iPos; i<(iPos + tamano); i++){
-            mem_address[i] = estados_address[pos_estado];
-        }
-        insertado = true;
-    }
-
-    if(insertado){
-    	//Escribe en bitácora
-    	sprintf(resultado, ": Proceso: Asignación. Hilo: %ld. Filas Ocupadas:%d-%d. Tamaño %d. Tiempo %d\n",
-             pid, iPos, iPos+tamano-1, tamano, tiempo);
-    	escBitacora(resultado);
-	
-        // devuelve el semáforo
-        sem_post(&sem_ready);
-
-        estados_address[pos_estado].estado = Ejecutando;
-
-        //printf("Ejecutando...\n");
-        sleep(1);         //para notarlo en el espia ¡NO BORRAR! 
-        printf("(Entró %ld: tamaño %d, tiempo %d)\n", pid, tamano, tiempo);
-        
-        sleep(tiempo);              // "Ejecuta"
-
-        // pide el semáforo
-        sem_wait(&sem_ready);
-
-    	//Escribe en bitácora
-    	sprintf(resultado, ": Proceso: Desasignación. Hilo: %ld. Filas Ocupadas:%d-%d. Tamaño %d. Tiempo %d\n",
-             pid, iPos, iPos+tamano-1, tamano, tiempo);
-    	escBitacora(resultado);
-
-        // devuelve los recursos
-        for(int i=iPos; i<(iPos + tamano); i++){
-            mem_address[i].pid = -1;
-        }
-
-    }else{
-        sprintf(resultado, ": Proceso: Defunción. El hilo %ld de tamaño %d, murió. Motivo: No encontró amor\n", pid, tamano);
-        printf("-El hilo %ld de tamaño %d, murió porque no encontró amor\n", pid, tamano);
-=======
 
     // control_address[0] = cantidad de lineas
     ocupado = false;
@@ -533,27 +180,6 @@ void * worst_fit(void * arg){
     int tamanoRestante = cant_pags_proceso; // Ir restando el tamano restante con los espacios libres y si al final del for no es cero, se mata el proceso.
     int cant_pags_mem = control_address[0];
     for(int i=0; i < cant_pags_mem; i++){
-        /*
-        if(ocupado == true){
-
-            if(mem_address[i].pid == -1){
-                ocupado = false;    
-                tamanoRestante--;
-                if (tamanoRestante == 0)
-                    break;
-            }
-
-        }
-        else if(mem_address[i].pid == -1){
-            ocupado = false;
-            tamanoRestante--;
-            if (tamanoRestante == 0)
-                break;
-        }
-        else{
-            ocupado = true;    
-        }
-        */
 
        if(mem_address[i].pid == -1){
             ocupado = false;    
@@ -627,7 +253,6 @@ void * worst_fit(void * arg){
              pid, cant_pags_proceso);
         printf("-El hilo %ld de tamaño %d, murió porque no encontró espacio\n",
                  pid, cant_pags_proceso);
->>>>>>> Revvro
         escBitacora(resultado);
     }
 
@@ -640,10 +265,11 @@ void * worst_fit(void * arg){
     return NULL;
 }
 
-<<<<<<< HEAD
+/*
+     Algoritmo Segmentacion
+*/
 
-=======
->>>>>>> Revvro
+
 int main()
 {
     //crea la llave
@@ -669,19 +295,11 @@ int main()
             || estados_address == (void*)-1){
             printf("No se puede apuntar a la memoria compartida\n");
         }else{
-<<<<<<< HEAD
-            int cant_lineas = control_address[0];
-=======
->>>>>>> Revvro
 
             int algoritmo = 0;
             while(algoritmo < 1 || algoritmo > 2){
                 printf("Algoritmos de asignación de memoria\n");
-<<<<<<< HEAD
-                printf("  1) Paginacion\n");
-=======
                 printf("  1) Procesocion\n");
->>>>>>> Revvro
                 printf("  2) Segmentacion\n");
                 printf("______________________________________\n");
                 printf("Seleccione el algoritmo a utilizar: ");
@@ -700,13 +318,6 @@ int main()
 
             // Así debería comportarse, maomeno
             while(control_address[1] == 1){         //mientras la memoria esté viva
-<<<<<<< HEAD
-                pthread_t proceso;
-                struct Proceso info_proceso;
-
-                info_proceso.tamano = getRandom(1, 10);
-                info_proceso.tiempo = getRandom(20, 60);
-=======
                 
                 pthread_t proceso;
                 struct Proceso info_proceso_pag;
@@ -714,37 +325,22 @@ int main()
                 control_address[3] = control_address[3] + 1; // Suma de cantidad de procesos para mantener el PID
                 info_proceso_pag.cant_pags = getRandom(1, 10);
                 info_proceso_pag.tiempo = getRandom(20, 60);
->>>>>>> Revvro
 
                 switch(algoritmo){
                     // Algoritmo de Paginacion
                     case 1:
-<<<<<<< HEAD
-                        pthread_create(&proceso, NULL, first_fit, (void *)&info_proceso);
-                        break;
-                    // Algoritmo de Segmentacion
-                    case 2:
-                        pthread_create(&proceso, NULL, best_fit, (void *)&info_proceso);
-=======
                         pthread_create(&proceso, NULL, asignarEspacio_Paginacion, (void *)&info_proceso_pag);
                         break;
                     // Algoritmo de Segmentacion
                     case 2:
                         pthread_create(&proceso, NULL, asignarEspacio_Paginacion, (void *)&info_proceso_pag);
->>>>>>> Revvro
                         break;
                 }
 
 
-<<<<<<< HEAD
-                int espera = getRandom(10, 20);
-                //int espera = getRandom(30, 60);
-                printf("Nuevo hilo en %d segundos\n", espera);
-=======
                 int espera = getRandom(15, 25);
                 printf("Nuevo hilo en %d segundos\n", espera);
 
->>>>>>> Revvro
                 sleep(espera);
             }
 
